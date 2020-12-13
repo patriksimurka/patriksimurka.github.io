6dbe9b7572446477590096af893173cd4edf9790
patriksimurka <49786855+patriksimurka@users.noreply.github.com>
2020-12-14 00:13:12 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,60 @@ function no_click() {
   click = false;
   setTimeout(function() {click = true;}, 700);  
 }
+
+function main(skupina) {
+  shuffle(orig_ziaci);
+
+  var storedNames = JSON.parse(localStorage.getItem(skupina));
+
+  if(storedNames === null || storedNames.length == 0) {
+    var ziaci = orig_ziaci;
+  } else {
+    var ziaci = storedNames;
+  }
+
+  for(i=ziaci.length+1; i<=30; i++){
+    j = i.toString();
+    document.getElementById(j).parentElement.style.display='none';
+  }
+
+  var uz = [];
+
+  var click = true;
+
+  $(document).on('click', 'button', function () {
+
+    if(click) {
+
+      revert();
+      
+      $(this).css({'background-color':"#6b027d","color":"white","font-weight":"900","width":"140px","margin-left":"auto"});
+      
+      ake = $( this ).find(".cislo").attr( "id" );
+      var uz_ziaci = [];
+      if(!uz.includes(ake)){
+        $( this ).find(".cislo").text( ziaci.pop() );
+        uz.push(ake);
+        var temp = ziaci.slice();
+        var vsetky = document.getElementsByClassName("cislo");
+        for (i=0; i<=vsetky.length-1; i++) {
+          if((vsetky[i]).innerHTML == '???'){
+            vsetky[i].style.fontSize = '12px';
+            vsetky[i].innerHTML = (temp.pop());
+            shuffle(temp);
+          }
+          setTimeout(function(){revert();}, 750);
+        } 
+        shuffle(ziaci);
+      };
+      localStorage.setItem(skupina, JSON.stringify(ziaci));
+      no_click();
+    }
+  });
+
+  document.getElementById("reset").onclick = function(){
+    localStorage.setItem(skupina, JSON.stringify(orig_ziaci));
+    location.reload();
+    return false;
+  }
+}
