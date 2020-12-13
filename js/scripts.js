e55b0a8995ef554e40ecf4595577c4d123954a8c
TacticalNuke4 <49786855+TacticalNuke4@users.noreply.github.com>
2020-12-13 15:59:47 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function revert() {
+  for(i=0; i<=30; i++){
+    var j = i.toString();
+    var tento = document.getElementById(j);
+    if(tento != null && tento.parentElement.style.color!='white'){
+      tento.style.fontSize = '18px';
+      tento.innerHTML = '???';
+    }
+  }
+}
